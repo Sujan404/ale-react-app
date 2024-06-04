27effa355ef0e831ae6bd4f6164797fa77647f2d
sujanale10 <sujanale404@gmail.com>
2024-06-05 01:28:28 +1000

    added new images

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import './intro.css';
 import bg from '../../assets/toy-story.png';
+import hireBtn from '../../assets/hire-button.png';
 import {Link} from 'react-scroll';
 const Intro = () => {
   return (
-    <section id='intro'>Hello,
+    <section id='intro'>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Toy Story</span> <br/> Website Designer</span>
-            <p className='introPara'>I am a skilled web designer with experience in creating visual websites</p>
-            <Link><button className='btn'><img src='' alt=''/></button></Link>
+            <p className='introPara'>I am a skilled web designer with experience in creating visually appealing and user friendly websites.</p>
+            <Link><button className='btn'><img src={hireBtn} alt='Hire Me' className='btnImg'/>Hire Me</button></Link>
         </div>
-        <img src={bg} alt='Profile' />
+        <img src={bg} alt='Profile' className='bg' />
     </section>
   )
 }
