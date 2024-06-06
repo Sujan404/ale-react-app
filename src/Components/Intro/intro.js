e76c9fb217daa950d34fd623e3ecaea66b25826e
sujanale10 <sujanale404@gmail.com>
2024-06-06 20:31:30 +1000

    add responsive css

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -3,17 +3,24 @@ import './intro.css';
 import bg from '../../assets/toy-story.png';
 import hireBtn from '../../assets/hire-button.png';
 import {Link} from 'react-scroll';
+import { Col, Row } from 'react-bootstrap';
 const Intro = () => {
   return (
+    <Row>
     <section id='intro'>
-        <div className='introContent'>
+      <Col lg={8} md={9} sm={9}>
+        <div className='introContent d-flex'>
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Toy Story</span> <br/> Website Designer</span>
-            <p className='introPara'>I am a skilled web designer with experience in creating <br/>visually appealing and user friendly websites.</p>
+            <p className='introPara'>I am a skilled web designer with experience in creating visually appealing and user friendly websites.</p>
             <Link><button className='btn'><img src={hireBtn} alt='Hire Me' className='btnImg'/>Hire Me</button></Link>
         </div>
+        </Col>
+        <Col lg={4} md={3} sm={3}>
         <img src={bg} alt='Profile' className='bg' />
+        </Col>
     </section>
+    </Row>
   )
 }
 
