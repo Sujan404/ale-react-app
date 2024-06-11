61b542c181e68e8a21c7995303ab06d961ac6823
sujanale10 <sujanale404@gmail.com>
2024-06-11 23:22:42 +1000

    change in test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react';
 
-test('renders learn react link', () => {
+test('renders react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Portfolio/i);
   expect(linkElement).toBeInTheDocument();
 });
